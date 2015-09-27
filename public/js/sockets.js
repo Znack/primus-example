0700a8b4d72f92a72892d6a05ecf3a80ba535a02
znack <scher56@gmail.com>
2015-09-27 10:17:34 +0600

    remove old example code from sockets handler

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -13,26 +13,13 @@ function log(name, header, message) {
   window.scroll(0, document.body.offsetHeight);
 }
 
-document.getElementById('action').onsubmit = function submit() {
-  if (!primus) {
-    log('error', 'Not connected', 'You need to press the `connect` button before you can send data');
-    return false;
-  }
-
-  try { primus.write((Function('return '+ document.getElementById('data').value))()); }
-  catch (e) { log('error', 'JavaScript Error', e.message); }
-
-  return false;
-};
-
-document.getElementById('connect').onclick = function click() {
+(function connect() {
   if (primus) primus.end();
 
   primus = new Primus();
 
   primus.on('reconnect', function reconnect(opts) {
     log('reconnect', 'Reconnecting', 'We are <strong>scheduling</strong> a new reconnect attempt. This is attempt <strong>'+ opts.attempt +'</strong> and will trigger a reconnect operation in <strong>'+ opts.scheduled +'</strong> ms.');
-    document.getElementById('connect').innerHTML = 'reconnecting';
   });
 
   primus.on('reconnect', function reconnect() {
@@ -49,7 +36,6 @@ document.getElementById('connect').onclick = function click() {
 
   primus.on('open', function open() {
     log('open', 'Open', 'The connection has been established.');
-    document.getElementById('connect').innerHTML = 'connected';
   });
 
   primus.on('error', function error(err) {
@@ -62,10 +48,9 @@ document.getElementById('connect').onclick = function click() {
 
   primus.on('end', function end() {
     log('end', 'End', 'The connection has ended.');
-    document.getElementById('connect').innerHTML = 'connect';
   });
 
   primus.on('close', function end() {
     log('close', 'close', 'We\'ve lost the connection to the server.');
   });
-}
+})();
