6f9b0f48192377e4900273b3178e710b4e251709
znack <scher56@gmail.com>
2015-09-27 10:40:33 +0600

    integrate tags-input

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,7 +1,26 @@
 angular.module('myApp').controller('mainController', function($scope, sockets){
-  $scope.socketState = {};
-  sockets.on('open', function open() {
-    $scope.$apply(function(){$scope.socketState.online = true;})
-  })
+  $scope.tags = [
+    { "id": 1, "name": "Tag1" },
+    { "id": 2, "name": "Tag2" },
+    { "id": 3, "name": "Tag3" },
+    { "id": 4, "name": "Tag4" },
+    { "id": 5, "name": "Tag5" },
+    { "id": 6, "name": "Tag6" },
+    { "id": 7, "name": "Tag7" },
+    { "id": 8, "name": "Tag8" },
+    { "id": 9, "name": "Tag9" },
+    { "id": 10, "name": "Tag10" }
+  ];
 
+  $scope.chosenTags = [{ "id": 1, "name": "Tag1" },];
+
+  $scope.loadTags = function(query) {
+    return $scope.tags;
+  };
+
+  $scope.$watch('chosenTags', function watcher(oldValue, newValue) {
+    if (oldValue.length !== newValue.length) {
+      sockets.write({action: 'NEW_TAGS', data: $scope.chosenTags})
+    }
+  })
 });
